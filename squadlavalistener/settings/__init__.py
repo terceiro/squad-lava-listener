1be92b79e92b1b5c75151718aa947cee2ee9bbcc
Antonio Terceiro <terceiro@softwarelivre.org>
2017-02-09 13:56:26 -0200

    settings: accept path to DB file as environment variable

--- squadlavalistener/settings/__init__.py
+++ squadlavalistener/settings/__init__.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'squadlavalistener.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.environ.get('DB_PATH', os.path.join(BASE_DIR, 'db.sqlite3')),
     }
 }
 
