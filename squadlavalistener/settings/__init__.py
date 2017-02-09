e7c1d8aed6d651e026f78209c04b699a152f4107
Antonio Terceiro <terceiro@softwarelivre.org>
2017-02-09 13:55:46 -0200

    settings: put DB file at the root

--- squadlavalistener/settings/__init__.py
+++ squadlavalistener/settings/__init__.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 SECRET_KEY = 'top-secret!'
 
