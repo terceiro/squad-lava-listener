22d65b0fea764d724103387e315b350f5aa42146
Antonio Terceiro <terceiro@softwarelivre.org>
2017-02-09 16:13:56 -0200

    Redirect to /admin from /

--- squadlavalistener/urls.py
+++ squadlavalistener/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('api.urls')),
+    url(r'', RedirectView.as_view(url='/admin/')),
 ]
