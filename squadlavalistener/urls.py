6a81fd41a7f1bab83ddcadd4c6b82824a4020c45
Antonio Terceiro <terceiro@softwarelivre.org>
2017-02-13 15:48:08 -0200

    squadlavalistener.urls: remove hardcoded URL

--- squadlavalistener/urls.py
+++ squadlavalistener/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.generic import RedirectView
+from django.urls import reverse
+from django.shortcuts import redirect
+
+
+def goto_admin(request):
+    return redirect(reverse('admin:index'))
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('api.urls')),
-    url(r'', RedirectView.as_view(url='/admin/')),
+    url(r'', goto_admin),
 ]
