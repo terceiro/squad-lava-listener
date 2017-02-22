a5b6a8506c9d5473a86ae2af9b0c57dcdbb2910a
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2017-02-22 22:32:00 +0000

    Removed default queue name
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

--- squadlavalistener/settings/__init__.py
+++ squadlavalistener/settings/__init__.py
@@ -137,7 +137,6 @@ CELERY_RESULT_BACKEND = 'rpc://'
 CELERY_RESULT_PERSISTENT = False
 CELERY_ACCEPT_CONTENT = ['json', 'pickle']
 CELERYBEAT_SCHEDULE_FILENAME = "/tmp/squadlavalistenr-celery-beat"
-CELERY_DEFAULT_QUEUE = "lava"
 
 CELERYD_LOG_FORMAT = '[%(asctime)s] %(levelname)s: %(message)s'
 CELERYD_TASK_LOG_FORMAT = '[%(asctime)s] %(levelname)s %(task_name)s: %(message)s'
