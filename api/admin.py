7b90600588711e2826cd8e690a16b05741a50080
Antonio Terceiro <terceiro@softwarelivre.org>
2017-03-27 16:39:20 -0300

    Submission: allow submission from admin

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from api.models import Pattern, SquadToken, Submission
+from api.tasks import submit_to_lava
 
 
 class PatternAdmin(admin.ModelAdmin):
@@ -11,8 +12,13 @@ class SquadTokenAdmin(admin.ModelAdmin):
     pass
 
 
+def submit(modeladmin, request, queryset):
+    for submission in queryset:
+        submit_to_lava.delay(submission.id)
+submit.short_description = 'Submit to LAVA'
+
 class SubmissionAdmin(admin.ModelAdmin):
-    pass
+    actions = [submit]
 
 
 admin.site.register(Pattern, PatternAdmin)
