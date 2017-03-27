dfd531edee4ea4ae1dc0adcdcb0eaf3caf8a204c
Antonio Terceiro <terceiro@softwarelivre.org>
2017-03-27 18:14:30 -0300

    Improve admin

--- api/admin.py
+++ api/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
 
 from api.models import Pattern, SquadToken, Submission
-from api.tasks import submit_to_lava
+from api.tasks import submit_to_lava, check_job_status
+
+
+def check_status(modeladmin, request, queryset):
+    for pattern in queryset:
+        check_job_status.delay(pattern, {
+            'job': pattern.lava_job_id,
+            'description': 'something',
+            'status': "Submitted",
+            'pipeline': True,
+        })
+check_status.short_description = 'Check LAVA for test status and results'
 
 
 class PatternAdmin(admin.ModelAdmin):
-    pass
+    actions = [check_status]
+    list_filter = ('is_active', 'lava_job_status', 'requester')
 
 
 class SquadTokenAdmin(admin.ModelAdmin):
@@ -19,6 +31,7 @@ submit.short_description = 'Submit to LAVA'
 
 class SubmissionAdmin(admin.ModelAdmin):
     actions = [submit]
+    list_filter = ('submitted', 'requester')
 
 
 admin.site.register(Pattern, PatternAdmin)
